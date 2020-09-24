fc1256e8d47b41fd1b0f78d938b3a50be07f9618
Mona Lisa <joannagrosshans2@gmail.com>
2020-09-24 09:48:34 -0500

    initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,19 @@
+var startBtn = document.querySelector('#start');
+var questions = [
+    {q:'', a:''}
+    {q:'', a:''}
+    {q:'', a:''}
+    {q:'', a:''}
+    {q:'', a:''}
+]
+
+
+//start function for quiz
+function start() {
+    //when button is clicked, first question is presented and timer starts
+    for (var i = 0; i < questions.length; i++) {
+        
+    }
+}
+
+startBtn.addEventListener("click", start);
