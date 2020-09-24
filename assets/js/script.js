697c16b63bcebe28820924b3f042dacb434e84a7
Mona Lisa <joannagrosshans2@gmail.com>
2020-09-24 13:15:15 -0500

    array questions updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,117 @@
 var startBtn = document.querySelector('#start');
+var timerEl = document.getElementById('countdown');
+
 var questions = [
-    {q:'', a:''}
-    {q:'', a:''}
-    {q:'', a:''}
-    {q:'', a:''}
-    {q:'', a:''}
-]
+    {
+        q:'A ____ is a predefined action that can be called or used later in the code.',
+        a: 'function',
+        b: 'alert',
+        c: 'prompt',
+        d: 'loop',
+        answer: 'a'
+    },
+    {
+        q:'In "function win() {}", "win" is the ______',
+        a: 'function keyword',
+        b: 'function name',
+        c: 'expression',
+        d: 'argument',
+        answer: 'b'
+    },
+    {
+        q:'The code between _____ is called a code block.',
+        a: 'parentheses',
+        b: 'brackets',
+        c: 'curly braces',
+        d: 'quotations',
+        answer: 'c'
+    },
+    {
+        q:'Which are examples of a data type?',
+        a:'string',
+        b: 'boolean',
+        c: 'integer',
+        d: 'all of the above',
+        answer: 'd'
+    },
+    {
+        q:'"var win = function() {}" is an example of', 
+        a:'function declaration',
+        b: 'function expression',
+        c: 'none of the above',
+        d: 'all of the above',
+        answer:'b'
+    },
+];
 
-
-//start function for quiz
-function start() {
-    //when button is clicked, first question is presented and timer starts
+// function elem(x) {
+//     return document.getElementById(x);
+// }
+function startTest() {
+    // test = elem("test");
+    //when button is clicked, timer begins
+    var score = 0;
+    //when button is clicked, ask questions one by one
     for (var i = 0; i < questions.length; i++) {
-        
-    }
-}
+        var answer = confirm(questions[i].q);
+        if 
+            (answer === true) 
+        {
+            //score increases with correct answer
+            score++;
+        }else {
+            score--;
+        }
+        }
+};
+// display question
 
+// display answer
+var btn = document.createElement("button");
+btn.innerHTML = "a";
+
+
+// //timer function
+// function timerCount() {
+//     // start time at 60 sec
+//     var timer = 60;
+//     //set timer to decrease
+//     var timeInterval = setInterval(function() {
+//         // if timer is above 0, then decrease by 1
+//         if (timer > 0) {
+//             timer--;
+//         // if incorrect answer, lose 10 sec
+//         } 
+//         // if timer is 0, stop interval
+//         else if (timer ===0) {
+//             clearInterval(timeInterval);
+//             timerEl.textContent = 'Quiz over';
+//             diplayMessage()
+//             highScore();
+//         }
+//     }, 1000);
+
+// }
+// //display message for answers
+// // function displayMessage(type, message) {
+
+// }
+//user input to high Score
+function highScore() {
+//your score is
+//input saved name
+//save to local storage
+
+}
+//start function for quiz
+
+//if answer correct, then told correct!
+//if answer incorrect, then told wrong!
+//if answer is incorrect, then 10 sec less on timer
+//when timer hits 0, timer stops
+//when questions are done, quiz over
+//high score input
+//store high score
+// start();
 startBtn.addEventListener("click", start);
+btn.addEventListener ("click", function() );
